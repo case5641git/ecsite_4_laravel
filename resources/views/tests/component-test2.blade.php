cf5873a9e07ca2b839ab78234eef3ea51eeefe8c
case5641git <t.hiroto.job@gmail.com>
2024-08-26 12:14:06 +0900

    sec02-multlogin

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+  <x-slot name="header">ヘッダー２</x-slot>
   test2
+  <x-test-class-base classBaseMessage="メッセージです"/>
+  <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値を変更"/>
 </x-tests.app>
